fc6a2888eb8df18a2705ce5f00a723ca0cd46790
saurav75-git <sauravpatel960895@gmail.com>
2025-01-06 17:12:12 +0530

    Commited on 05-01-2025

---
+++ krischoWeb/krischoEnginner/js/pop.js
@@ -0,0 +1,42 @@
+$('#viveseries').on('click', function () {
+    $('.vive').css({ 'display': 'flex' })
+  })
+
+$('#vive-close').on('click', function(){
+    $('.vive').css({ 'display': 'none' })
+})
+
+
+$('#kiwi-self-primering-pump').on('click', function () {
+    $('.kiwi').css({ 'display': 'flex' })
+  })
+
+$('#kiwi-close').on('click', function(){
+    $('.kiwi').css({ 'display': 'none' })
+})
+
+
+$('#turtle-series-pump').on('click', function () {
+    $('.turtle').css({ 'display': 'flex' })
+  })
+
+$('#turtle-close').on('click', function(){
+    $('.turtle').css({ 'display': 'none' })
+})
+
+
+$('#tiger-self-primering-pump').on('click', function () {
+    $('.tiger').css({ 'display': 'flex' })
+  })
+
+$('#tiger-close').on('click', function(){
+    $('.tiger').css({ 'display': 'none' })
+})
+
+$('#1HP-Hippo').on('click', function () {
+    $('.hp1').css({ 'display': 'flex' })
+  })
+
+$('#hp1-close').on('click', function(){
+    $('.hp1').css({ 'display': 'none' })
+})
